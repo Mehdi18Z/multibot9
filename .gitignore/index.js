5446af24706fb5b9398af18d31b673f9af3f723c
Mehdi18Z <36314429+Mehdi18Z@users.noreply.github.com>
2018-02-09 22:36:58 +0000

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,1 +1,10 @@
+const Discord = require("discord.js");
 
+var bot = new Discord.Client();
+
+bot.on("ready",function() {
+   bot.user.setGame("MulTibot, !help");
+   console.log("le bot a bien ete connecte");
+});
+
+bot.login("NDEwODU1MTU1NzI3MjA0MzUy.DV-wYQ.UxudEl7n0exFrnZg3sz5dnGdaIY")
